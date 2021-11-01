ec90bfcf3cc205501168d543b229872c71b5f49d
Jay Gordon <jaydestro@gmail.com>
2021-11-01 10:52:47 -0400

    fixed hello world

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World, I am making change!');
+  res.send('Hello World!');
 });
 app.listen(8080, function () {
   console.log('Example "Hello World" app!');
