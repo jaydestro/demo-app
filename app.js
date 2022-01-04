52ba7448ae322ec5f20c0a69eb323b54d6e767a9
jay gordon <jaydestro@gmail.com>
2022-01-04 15:11:42 +0000

    new

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-const http = require('http');
-const port = process.env.PORT || 8080;
-
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World! We're live!');
+  res.send('Hello World it's Jay!');
 });
 app.listen(8080, function () {
-  console.log('Example "Hello World" app!');
+  console.log('Example "Hello World" app listening on port 8000!');
 });
