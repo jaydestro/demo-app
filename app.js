5bb0eb30787116a5243b17553d646bde92b3088d
jay gordon <jaydestro@gmail.com>
2022-01-04 15:06:31 +0000

    build

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! We're live!');
 });
 app.listen(8080, function () {
   console.log('Example "Hello World" app!');
