63b8cba0805375c7074e76934a90935a02b8e6d6
Jay Gordon <jaydestro@gmail.com>
2022-08-11 10:52:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World from Jay!');
+  res.send('Hello World!');
 });
 app.listen(8080, function () {
   console.log('Example "Hello World" app listening on port 8080!');
 });
+
+let current = new Date();
+let cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
+let cTime = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
+let dateTime = cDate + ' ' + cTime;
+console.log(dateTime);
