e066c3ae6880e6bc19416cb4e30b0ee802780123
Jay Gordon <jaydestro@gmail.com>
2022-08-11 11:29:27 -0400

    minor changes

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(8080, function () {
-  console.log('Example "Hello World" app listening on port 8080!');
-});
-
 let current = new Date();
 let cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
 let cTime = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
 let dateTime = cDate + ' ' + cTime;
 console.log(dateTime);
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!\n'+ (dateTime));
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
