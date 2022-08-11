2236156ed3ebfcc52f09a0ae2d249311b52ed5f6
Jay Gordon <jaydestro@gmail.com>
2022-08-11 12:07:42 -0400

    test

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n'+ (dateTime));
+  res.end('Hello World!');
 });
 
 server.listen(port, hostname, () => {
